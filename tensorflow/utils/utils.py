2cefdab0797e6f7826215e82fd94ac0c5adcc5ee
rluver <skyd0608@gmail.com>
2022-05-26 20:10:42 +0900

    Implements mask_future_data

--- tensorflow/utils/utils.py
+++ tensorflow/utils/utils.py
@@ -6,3 +6,12 @@ def is_padded(x: tf.Tensor) -> tf.Tensor:
     masking = masking[:, tf.newaxis, tf.newaxis, :]
     
     return masking
+
+
+def mask_future_data(x: tf.Tensor) -> tf.Tensor:
+    seq_len = x.shape[1]
+    square_matrix = tf.ones((seq_len, seq_len))
+    future_masking_matrix = 1 - tf.linalg.band_part(square_matrix, -1, 0)   # upper triangle except diagonal
+    padding_check_matrix = is_padded(x)
+
+    return tf.maximum(future_masking_matrix, padding_check_matrix)
